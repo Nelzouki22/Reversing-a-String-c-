14f254156beed27df8edff268bc69d675ea2216f
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-23 23:37:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hello, World!";
+        string reversed = ReverseString(input);
+
+        Console.WriteLine("Original String: " + input);
+        Console.WriteLine("Reversed String: " + reversed);
+    }
+
+    static string ReverseString(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
